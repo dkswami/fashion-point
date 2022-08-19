62c9599211eb4548f00cadbfd5ec07eb30935ea4
dkswami <dkswami1820@gmail.com>
2022-08-19 16:19:20 +0530

    react router implemented and nav bar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Products from './components/products/products.component';
+import Navigation from './routes/navigation/navigation.component';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+const Home = () => {
+  return(
+    <>
+    <h1>this is a demo website for simmi foundation by deepak swami</h1>
+    </>
+  )
+}
+
+const Authentication = () => {
+  return(
+    <>
+      <h2> Sign in or sign up</h2>
+    </>
+  )
+}
+
+
 function App() {
   return (
-    <div className="App">
-      <Products/>
-    </div>
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='products' element={<Products/>} />
+        <Route path='auth' element={<Authentication />} />
+      </Route>
+    </Routes>
   );
 }
 
