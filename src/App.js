28991360e04899f90b5b62286e80b60cc5ac4ee9
dkswami <dkswami1820@gmail.com>
2022-08-19 12:21:12 +0530

    product card and products components added and displayed properly

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from './logo.svg';
+import Products from './components/products/products.component';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Products/>
     </div>
   );
 }
